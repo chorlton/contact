bbddd0274d270fa351eaffbe8591dc72ffd7bc85
unknown <gordon@.(none)>
2009-08-03 21:22:59 +0100

    got telephone number saves working with polymorphic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :contacts
+  map.resources :contacts do |contacts|
+    contacts.resources :telephone_numbers, :controller => 'telephone_numbers'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
 
